ffb33cae952c2fc7dfed0e9b6085d24b4f11312c
Mitch Holm <mitch@Mitchs-MacBook-Pro.local>
2016-06-04 15:21:04 -0400

    mainjs

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var geolib = require("geolib");
 
-var locs = [];
+var locs = [{latitude: 30, longitude: 40}];
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');

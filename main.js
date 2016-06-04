8c02ef77436bc938a39b0e1f878d6c66451fbdc4
Mitch Holm <mitch@Mitchs-MacBook-Pro.local>
2016-06-04 15:20:21 -0400

    mainjs

--- main.js
+++ main.js
@@ -13,32 +13,33 @@ app.get('/', function (req, res) {
 });
 
 app.post('report', function(req,res) {
-	console.log(res.body);
-	var date = new Date();
-	var subTime = date.now();
-	var userloc = { latitude: res.body.latitude,
-					longitude:res.body.longitude,
+	console.log(req.body);
+	var subTime = Date.now();
+	var userloc = { latitude: req.body.latitude,
+					longitude:req.body.longitude,
 					 time: subTime}
 	locs.push(userloc);
 	res.send("thanks bro");
 })
 
 app.post('find', function(req,res) {
-	console.log(res.body);
-	var quan = res.body.amount;
-	var lat = res.body.lat;
-	var lon = res.body.long;
+	console.log(req.body);
+	var quan = req.body.amount;
+	var lat = req.body.lat;
+	var lon = req.body.long;
 	var user;
+	if(locs.length = 0)
+		res.send("YOU'RE OUT OF LUCK COWBOY");
 	user.latitude = lat;
 	user.longitude = lon;
 	var nearest = geolib.findNearest(user,locs,1);
 	res.send(nearest);
 })
 
-var cull = setInterval(function(){
-	var date = new Date();
-	var now = date.now();
-	if(now != locs[0].time + 3600000){
-		locs.splice(0,1);
-	}
-},5000)
+// var cull = setInterval(function(){
+// 	var date = new Date();
+// 	var now = date.now();
+// 	if(now != locs[0].time + 3600000){
+// 		locs.splice(0,1);
+// 	}
+// },5000)

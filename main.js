0cf6a387133a564e60e205597b34255b1558b9d9
Mitch Holm <mitch@Mitchs-MacBook-Pro.local>
2016-06-04 14:51:55 -0400

    swag

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var app = express();
-//var Firebase = require("firebase");
 var geolib = require("geolib");
-// var config = {
-//     apiKey: "AIzaSyCQ_blsuxK_BSeW1J7y0yIAqQPgdBA1pE0",
-//     authDomain: "hondacivichack.firebaseapp.com",
-//     databaseURL: "https://hondacivichack.firebaseio.com",
-//     storageBucket: "hondacivichack.appspot.com",
-//   };
-//   firebase.initializeApp(config);
-//var myFirebaseRef = new Firebase("https://hondacivichack.firebaseio.com/");
+
+var locs = [];
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
@@ -21,25 +14,31 @@ app.get('/', function (req, res) {
 
 app.post('report', function(req,res) {
 	console.log(res.body);
+	var date = new Date();
+	var subTime = date.now();
+	var userloc = { latitude: res.body.latitude,
+					longitude:res.body.longitude,
+					 time: subTime}
+	locs.push(userloc);
 	res.send("thanks bro");
-	var quan = res.body.quantity;
-	//var loc = myFirebaseRef.child("location");
-
 })
 
 app.post('find', function(req,res) {
 	console.log(res.body);
 	var quan = res.body.amount;
-	res.send("thanks dude");
 	var lat = res.body.lat;
 	var lon = res.body.long;
 	var user;
 	user.latitude = lat;
 	user.longitude = lon;
-	// myFirebaseRef.on("location", function(snapshot) {
-	//   console.log(snapshot.val());
-	//   var nearest = geolib.findNearest(user,snapshot.val,1);
-	//   console.log(nearest);
-	//   res.send(nearest);
-	// });
+	var nearest = geolib.findNearest(user,locs,1);
+	res.send(nearest);
 })
+
+var cull = setInterval(function(){
+	var date = new Date();
+	var now = date.now();
+	if(now != locs[0].time + 3600000){
+		locs.splice(0,1);
+	}
+},5000)
